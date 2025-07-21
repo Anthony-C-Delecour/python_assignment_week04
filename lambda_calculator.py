0c42358cd1197568620dc13f596c49924a1b9b82
Anthony-C-Delecour <67110027@kmitl.ac.th>
2025-07-21 15:55:23 +0700

    .\.git\

--- lambda_calculator.py
+++ lambda_calculator.py
@@ -1,13 +1,20 @@
-add = lambda a, b: a + b
-subtract = lambda a, b: a - b
-multiply = lambda a, b: a * b
-divide = lambda a, b: a / b if b != 0 else "Error: Not Divisable by zero"
+def add(a, b):
+    return (lambda x, y: x + y)(a, b)
+
+def subtract(a, b):
+    return (lambda x, y: x - y)(a, b)
+
+def multiply(a, b):
+    return (lambda x, y: x * y)(a, b)
+
+def divide(a, b):
+    return (lambda x, y: x / y if y != 0 else "Error: Not Divisable by zero")(a, b)
 
 if __name__ == "__main__":
     x = 10
     y = 5
 
-    print(add(x, y))       
-    print(subtract(x, y)) 
-    print(multiply(x, y)) 
-    print(divide(x, y))     
+    print(add(x, y))
+    print(subtract(x, y))
+    print(multiply(x, y))
+    print(divide(x, y)) 
